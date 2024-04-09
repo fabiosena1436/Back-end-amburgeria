4055ebea26c5a7664cd60cc984eb1118fef60569
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-09 15:38:30 -0300

    Iniciando o projeto do DevBurger

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const routes = new Router();
+
+routes.get("/", (request, response) => {
+  return response.status(200).json({ message: "Hello world!" });
+});
+
+
+module.exports = routes
